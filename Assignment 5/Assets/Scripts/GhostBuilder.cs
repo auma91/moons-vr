9ea4952b6cd0357ed48daf6072358028dc46bd29
Aurangzeb Malik <auma9171@colorado.edu>
2020-10-04 12:27:36 -0600

    assingment 5

---
+++ Assignment 5/Assets/Scripts/GhostBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GhostBuilder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    public int count;
+    public int killed = 0;
+    public float time;
+    public Text txt;
+    void Start()
+    {
+
+    }
+    public void Killed() { killed++; txt.text = "Killed: " + killed.ToString(); }
+    public void DestroyCount() { count-=1; }
+    public int RandomSign() {
+        return Random.value < .5 ? 1 : -1;
+    }
+// Update is called once per frame
+void Update()
+    {
+        Debug.Log(count);
+        if (count < 2 && Random.value > 0.7 && time > 3)
+        {
+            var child = Instantiate(prefab, new Vector3(RandomSign()*Random.Range(10f, 29f), 1.15f, RandomSign()*Random.Range(10f, 30.0f)), Quaternion.identity, transform.parent);
+            child.transform.SetParent(transform);
+            count++;
+            time = 0f;
+        }
+        else { time += Time.deltaTime; }
+        if (count == 0)
+        {
+            var child = Instantiate(prefab, new Vector3(RandomSign()*Random.Range(10f, 29f), 1.15f, RandomSign()*Random.Range(10f, 30.0f)), Quaternion.identity, transform.parent);
+            child.transform.SetParent(transform);
+            count++;
+            time = 0f;
+        }
+        else { time += Time.deltaTime; }
+
+    }
+}
---
+++ Assignment 5/Assets/Scripts/instantiate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float angle;
+    public float speed = 2f;
+    public AudioClip SoundToPlay;
+    AudioSource audio;
+    private float convertDeg = 180 / Mathf.PI;
+    private float convertRad = Mathf.PI / 180;
+    public Vector3 init;
+    void Start()
+    {
+        init = transform.position;
+        //transform.LookAt(target);
+        float opposite = transform.position.x;
+        float adjacent = transform.position.z;
+        float hyptonenuse = Mathf.Sqrt(Mathf.Pow(opposite,2) + Mathf.Pow(adjacent, 2));
+        /*if (opposite <= 0) {
+            Mathf.in
+        }
+        else { }*/
+        
+        //Debug.Log(opposite + " " + adjacent);
+        angle = convertDeg * Mathf.Asin(opposite/hyptonenuse);
+        //Debug.Log(angle);
+        if (adjacent <=0) {
+            if (adjacent <= 0) { transform.localEulerAngles = new Vector3(0, -90 + (-1 * angle), 0); }
+            else { transform.eulerAngles = new Vector3(0, -90 + angle, 0); }
+        }
+        else {
+            if (adjacent <= 0) { transform.localEulerAngles = new Vector3(0, 90 + (-1 * angle), 0); }
+            else { transform.eulerAngles = new Vector3(0, 90 + angle, 0); }
+        }
+
+        audio = GetComponent<AudioSource>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x) > 0.5 || Mathf.Abs(transform.position.z)  > 0.5) {
+            Vector3 pos = new Vector3(Mathf.Sign(transform.position.x) * -1 * (speed) * Mathf.Abs(Mathf.Sin(convertRad * angle)), 0, Mathf.Sign(transform.position.z) * -1 * (speed) * Math.Abs(Mathf.Cos(convertRad * angle)));
+            transform.Translate(pos * Time.deltaTime, Space.World);
+        }
+    }
+
+    public void PlaySound() { audio.PlayOneShot(SoundToPlay); }
+    public void RemoveGhost() {
+        gameObject.GetComponentInParent<GhostBuilder>().Killed();
+        gameObject.GetComponentInParent<GhostBuilder>().DestroyCount();
+        GameObject.Destroy(gameObject); }
+    public void Seen() { Debug.Log("Seen"); }
+}
