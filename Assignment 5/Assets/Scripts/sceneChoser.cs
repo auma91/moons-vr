552d110386dbb704c2946c118c24ec61fa3a9d74
Aurangzeb Malik <auma9171@colorado.edu>
2020-12-08 23:41:20 -0700

    alot of new things

---
+++ Assignment 5/Assets/Scripts/sceneChoser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneChoser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void chooseLagoon() { SceneManager.LoadScene(2); }
+    public void chooseForest() {
+        SceneManager.LoadScene(1);
+    }
+}
